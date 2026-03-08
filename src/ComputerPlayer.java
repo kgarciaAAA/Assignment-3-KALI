57e7b948c6bde96e68f053985dc49514e57b498c
kgarciaAAA <ken.garcia1019@gmail.com>
2026-03-08 14:55:45 -0700

    Implemented ComputerPlayer getPlayerMove().

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -1,5 +1,24 @@
+import java.util.Random;
+
 public class ComputerPlayer extends Player{
+    private final Random randomNum = new Random();
+
+    /**
+     * Uses the random library to generate a random number from 0-2,
+     * and returns a Move based on the number selected.
+     * @return the computer's randomly selected move
+     */
+    @Override
     public Move getPlayerMove() {
-        return Move.PAPER;
+        int computerMove = randomNum.nextInt(3);
+
+        switch (computerMove) {
+            case 0:
+                return Move.ROCK;
+            case 1:
+                return Move.PAPER;
+            default:
+                return Move.SCISSORS;
+        }
     }
 }
